8457b50d6d8bc4e22d9d87fee41c945de47ecd2c
Evgeniy Stepanov <ziro.ter@yandex.ru>
2025-02-07 17:13:15 +0400

    For test 07.02 v2

--- the_snake.py
+++ the_snake.py
@@ -80,7 +80,7 @@ class Apple(GameObject):
     Яблоко появляется в случайной позиции и может быть съедено змейкой.
     """
 
-    def __init__(self, occupied_positions, color=APPLE_COLOR):
+    def __init__(self, occupied_positions=None, color=APPLE_COLOR):
         super().__init__(color)
         self.randomize_position(occupied_positions)
 
